3e2bf10b02b75c61e997c4c1581cfecdba4b9659
nicole-kwon <kwonn@bu.edu>
2021-09-14 10:36:32 -0400

    Creating set-up for Firebase API

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'dart:convert';
 import 'package:miniproject1/models/results.dart';
 
+import 'package:english_words/english_words.dart';
+
 GoogleSignIn _googleSignIn = GoogleSignIn();
 
 void main() {
@@ -162,7 +164,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: IconButton(
                         onPressed: () {
                           // ADD LINE OF CODE BELOW OF THIS COMMENT
-                          // Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new MySearcher()));
+                          Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new RandomWords()));
                           //var result = await search(searchterm);
                           //Welcome welcome = new Welcome.fromJson(json.decode(result.body));
                           //Example of parsing
@@ -236,7 +238,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
 
-// Search Bar
+
+
+
+
+
+
+
 
 
 
@@ -315,7 +323,133 @@ class _MyScannerState extends State<MyScanner> {
                           child: Text('Start barcode scan')),
                       Text('\nName: $_scanName \n\nIngredients: $_scanDescription \n',
                           style: TextStyle(fontSize: 20))
-                    ]));
+                    ])
+            );
           }));
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Search Bar
+class RandomWords extends StatefulWidget {
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  // #enddocregion RWS-var
+
+  // #docregion _buildSuggestions
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return const Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+  // #enddocregion _buildSuggestions
+
+  // #docregion _buildRow
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+  // #enddocregion _buildRow
+
+  // #docregion RWS-build
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          ),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  // #enddocregion RWS-build
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // NEW lines from here...
+        builder: (context) {
+          final tiles = _saved.map(
+                (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        }, //...to here.
+      ),
+    );
+  }
+// #docregion RWS-var
+}
+// #enddocregion RWS-var
